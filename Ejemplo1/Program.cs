fd5affd68b5afea326c0f34c7b3e2a7a711dba8d
adria <adria@DESKTOP-Q4P4BOO>
2020-10-08 10:51:06 +0200

    primer commit

---
+++ Ejemplo1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Ejercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String x=" ";
+
+            Console.WriteLine("Hello World!");
+            /*Console.ReadLine();*/
+            
+        }
+    }
+}
